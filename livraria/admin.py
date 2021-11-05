3ec04b5ae73413223992b1c5fd02de0ce5dff040
Claudionice Rosah <rosahclaudionice@gmail.com>
2021-11-05 11:05:26 -0300

    Configurando o Admin

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from livraris.models import livros, categoria
+
+class LivrosAdmin (admin.ModelAdmin):
+    list_display = ('id', 'Titulo', 'Autor', 'Editora', 'Data_Publicacao')
+    list_display = ('id', 'Titulo')
+    search_fields = ('Titulo',)
+    list_per_page = 20
+
+admin.site.register (livros, LivrosAdmin)
+
+
+
+class CategoriaAdmin (admin.ModelAdmin):
+    list_display = ('id', 'Genero', 'Contra_Capa')
+    search_fields = ('Genero',)
+
+
+admin.site.register (Categoria, CategoriaAdmin)
+
 
 # Register your models here.
