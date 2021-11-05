690dae36b58cebcf2b29b2a3ade5aacd5fb7118f
Claudionice Rosah <rosahclaudionice@gmail.com>
2021-11-05 10:49:10 -0300

    Models livros, categoria

--- livraria/models.py
+++ livraria/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class livros (models.Model):
+    Titulo = models.CharField (max_length = 60)
+    Autor = models.CharField (max_length = 30)
+    Editora = models.CharField (max_length = 30)
+    Data_Publicacao = models.DateField ()
+
+    def __str__ (self):
+        return self.Titulo
+
+
+class categoria (models.Model):
+    Genero = models.CharField (max_length = 30)
+    Contra_Capa = models.TextField()
+
+    def __str__ (self):
+        return self.Genero
+
+
+
 # Create your models here.
